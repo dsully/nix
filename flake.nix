1e83c4790a5937522d8ead1cc11446a5e9bc87b7
Dan Sully <dsully@users.noreply.github.com>
2025-05-08 05:43:39 -0700

    chore: update nix-darwin uri

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-index.url = "github:nix-community/nix-index";
