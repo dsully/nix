1a26266a2bf6d63576851be6c710e212090e4d06
Dan Sully <dsully@users.noreply.github.com>
2025-06-07 13:14:43 -0700

    fix(direnv): apply patch that fixes the output message

--- lib/common/home/packages/system.nix
+++ lib/common/home/packages/system.nix
@@ -17,7 +17,17 @@
       curl
       dasel
       delta
-      direnv
+      (pkgs.direnv.overrideAttrs (oldAttrs: {
+        doCheck = false;
+        patches =
+          (oldAttrs.patches or [])
+          ++ [
+            (pkgs.fetchpatch {
+              url = "https://github.com/direnv/direnv/pull/1426.patch";
+              sha256 = "sha256-qK4wT4+jZdzLBWm5m0up/VZWiUw9kzS9FhM0NsC/DZo=";
+            })
+          ];
+      }))
       dua
       dust
       fclones
