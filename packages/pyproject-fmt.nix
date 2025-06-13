1b7de3536ee2032660d1f17cc63aaa5b2ceb5b52
Dan Sully <dsully@users.noreply.github.com>
2025-06-13 09:15:10 -0700

    fix: 'format = wheel' doesn't work.

--- packages/pyproject-fmt.nix
+++ packages/pyproject-fmt.nix
@@ -1,15 +1,40 @@
-{pkgs, ...}:
+{
+  perSystem,
+  pkgs,
+  ...
+}:
 with pkgs;
   python3.pkgs.buildPythonApplication rec {
     pname = "pyproject-fmt";
     version = "2.6.0";
-    format = "wheel";
+    pyproject = true;
 
     src = fetchPypi {
       pname = "pyproject_fmt";
       version = "2.6.0";
       hash = "sha256-ZkCDD1n2XSaqlT9c6IfSO5NZhWpdDhDTPHVrlnayKd8=";
     };
+
+    cargoDeps = rustPlatform.fetchCargoVendor {
+      inherit src;
+      name = "${pname}-${version}";
+      hash = "sha256-vK4+Pgi1kCilBm7RhWTCom/Q3u/DHf6LTbRmQI+8IqU=";
+    };
+
+    build-system = [
+      cargo
+      rustPlatform.cargoSetupHook
+      rustPlatform.maturinBuildHook
+      rustc
+    ];
+
+    dependencies = [
+      perSystem.self.toml-fmt-common
+    ];
+
+    pythonImportsCheck = [
+      "pyproject_fmt"
+    ];
 
     meta = {
       description = "Format your pyproject.toml file";
