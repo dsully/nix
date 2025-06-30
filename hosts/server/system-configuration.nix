d4df719ec796f18ab552a8e1dc2016eb32743dcd
Dan Sully <dsully@users.noreply.github.com>
2025-06-29 17:37:53 -0700

    fix: linux / system-manager

--- hosts/server/system-configuration.nix
+++ hosts/server/system-configuration.nix
@@ -1,28 +1,19 @@
 {
-  flake,
   pkgs,
   system-manager,
   ...
 }: {
   config = {
-    imports = [
-      flake.modules.common.chsh
-    ];
-
-    chsh.users."${flake.lib.defaultUser}" = pkgs.fish;
-
     environment = {
       etc = {
-        "sudoers.d/10-nix-commands".text = let
-          commands = [
-            "/nix/store/*/activate"
-            "/nix/var/nix/profiles/default/bin/nix*"
-            "/run/system-manager/sw/bin/liquidctl"
-            "/run/system-manager/sw/bin/system-manager"
-          ];
-          commandsString = builtins.concatStringsSep ", " commands;
-        in ''
-          %admin ALL=(ALL:ALL) NOPASSWD:SETENV ${commandsString}
+        "sudoers.d/10-nix-commands".text = ''
+          Cmnd_Alias SYSTEM_MANAGER = ${system-manager}/bin/system-manager (build|switch) *
+          Cmnd_Alias LIQUIDCTL = ${pkgs.liquidctl}/bin/liquidctl (list|status)
+          Cmnd_Alias NIX = ${pkgs.lix}/bin/nix *
+
+          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:LIQUIDCTL
+          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:NIX
+          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:SYSTEM_MANAGER
 
           Defaults:%sudo env_keep += "PATH SSH_AGENT_PID SSH_AUTH_SOCK TERM TERMINFO"
         '';
