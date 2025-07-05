f1cc5480ae5fcef78ff68e69472a65cafb3d47bf
Dan Sully <dsully@users.noreply.github.com>
2025-07-05 07:33:21 -0700

    feat: add codebook

--- modules/home/packages/development.nix
+++ modules/home/packages/development.nix
@@ -7,6 +7,18 @@
     packages = with (pkgs // ((flake.inputs.upstream or flake).packages.${pkgs.system} or {}));
       [
         claude-code
+        # Until 0.34+ hits nixpkgs
+        (pkgs.codebook.overrideAttrs (oldAttrs: {
+          doCheck = false;
+          patches =
+            (oldAttrs.patches or [])
+            ++ [
+              (pkgs.fetchpatch {
+                url = "https://github.com/blopker/codebook/pull/86.patch";
+                sha256 = "sha256-s4GrIbyYho+znGuXljNr+AIB6ulBKZKRHA8OS+3EBms=";
+              })
+            ];
+        }))
         cyme
         dive
         fork-cleaner
