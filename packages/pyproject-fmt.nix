fad3fbcaf1da58d1cd1bb743a2b6262b91dccc68
Dan Sully <dsully@users.noreply.github.com>
2025-07-16 19:18:41 -0700

    chore: simplify pyproject-fmt

--- packages/pyproject-fmt.nix
+++ packages/pyproject-fmt.nix
@@ -1,39 +1,50 @@
-{
-  perSystem,
-  pkgs,
-  ...
-}:
-with pkgs;
-  python3.pkgs.buildPythonApplication rec {
-    pname = "pyproject-fmt";
-    version = "2.6.0";
-    pyproject = true;
+{pkgs, ...}:
+with pkgs; let
+  dists = {
+    aarch64-darwin = {
+      platform = "macosx_11_0_arm64";
+      hash = "sha256-qzQBXGjcyk1MqPMJ/ntf71SDRNB/MjH6PcrnBuagIdQ=";
+    };
+    x86_64-linux = {
+      platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+      hash = "sha256-iRngVSVSl5ccEs8CvOfRK5X9AhUZAQVN9SSy6AutSUg=";
+    };
+  };
+
+  d = dists.${system} or (throw "Unsupported system: ${system}");
+
+  toml-fmt-common = python3.pkgs.buildPythonPackage rec {
+    pname = "toml-fmt-common";
+    version = "1.0.1";
+    format = "wheel";
 
     src = fetchPypi {
+      abi = "none";
+      dist = "py3";
+      hash = "sha256-emVC42pxZ/qUuLmX0/jeutu0q3V8fXincwRXm9egzH0=";
+      inherit format;
+      pname = "toml_fmt_common";
+      python = "py3";
+      inherit version;
+    };
+  };
+in
+  python3.pkgs.buildPythonPackage rec {
+    pname = "pyproject-fmt";
+    version = "2.6.0";
+    format = "wheel";
+
+    src = fetchPypi {
+      abi = "abi3";
+      dist = "cp39";
+      inherit (d) hash platform;
+      inherit version format;
       pname = "pyproject_fmt";
-      version = "2.6.0";
-      hash = "sha256-ZkCDD1n2XSaqlT9c6IfSO5NZhWpdDhDTPHVrlnayKd8=";
+      python = "cp39";
     };
 
-    cargoDeps = rustPlatform.fetchCargoVendor {
-      inherit src;
-      name = "${pname}-${version}";
-      hash = "sha256-vK4+Pgi1kCilBm7RhWTCom/Q3u/DHf6LTbRmQI+8IqU=";
-    };
-
-    build-system = [
-      cargo
-      rustPlatform.cargoSetupHook
-      rustPlatform.maturinBuildHook
-      rustc
-    ];
-
-    dependencies = [
-      perSystem.self.toml-fmt-common
-    ];
-
-    pythonImportsCheck = [
-      "pyproject_fmt"
+    propagatedBuildInputs = [
+      toml-fmt-common
     ];
 
     meta = {
