1474c584e091d2a6879395973931ee9b0257c7f5
Dan Sully <dsully@users.noreply.github.com>
2025-07-22 08:14:44 -0700

    chore: update opencode version

--- packages/opencode-updated.nix
+++ packages/opencode-updated.nix
@@ -3,11 +3,11 @@ with pkgs; let
   platformData = {
     "x86_64-linux" = {
       filename = "opencode-linux-x64.zip";
-      hash = "sha256-PMHzk6vk27JuKabdoxAIaDnYtYldM/DKWhBmKMP5y2s=";
+      hash = "sha256-4OmTB+B1E5c56crab91uWMiSGesQM1qUc4sL8v97eMg=";
     };
     "aarch64-darwin" = {
       filename = "opencode-darwin-arm64.zip";
-      hash = "sha256-heaHMdf4CDoWt13HySc4Tu/B4n+a7OuKKYbngjp/aNM=";
+      hash = "sha256-EY5CQOM90AZRrkF4xuaSdNgTzAJyWwGh/zgQ3Zmsfs4=";
     };
   };
 
@@ -15,7 +15,7 @@ with pkgs; let
 in
   stdenvNoCC.mkDerivation rec {
     pname = "opencode-updated";
-    version = "0.3.54";
+    version = "0.3.55";
 
     src = fetchurl {
       url = "https://github.com/sst/opencode/releases/download/v${version}/${currentPlatform.filename}";
