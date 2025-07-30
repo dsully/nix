0875271cf81b04a914f383bacb02edef14cdb2a6
Dan Sully <dsully@users.noreply.github.com>
2025-07-30 08:39:46 -0700

    chore: update lockfile

---
+++ packages/zuban.nix
@@ -0,0 +1,34 @@
+{pkgs, ...}:
+with pkgs; let
+  dists = {
+    aarch64-darwin = {
+      platform = "macosx_11_0_arm64";
+      hash = "sha256-2dk+3tMI1E4Qot6ppCcs24GXBus7Sw/sAudOPRbh6x0=";
+    };
+    x86_64-linux = {
+      platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+      hash = "sha256-NVB17TmY0xrHOe7udrWfSTrBdw+VTok1D/7LecbqiJQ=";
+    };
+  };
+
+  d = dists.${system} or (throw "Unsupported system: ${system}");
+in
+  python3.pkgs.buildPythonPackage rec {
+    pname = "zuban";
+    version = "0.0.17";
+    format = "wheel";
+
+    src = fetchPypi {
+      inherit pname version format;
+      inherit (d) hash platform;
+      abi = "none";
+      python = "py3";
+      dist = "py3";
+    };
+
+    meta = {
+      description = "A Mypy-compatible Python Language Server built in Rust";
+      homepage = "https://pypi.org/project/zuban/";
+      mainProgram = pname;
+    };
+  }
