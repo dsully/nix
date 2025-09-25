247c6d74598e53d02a70c32d0f35b4397c4c9920
Dan Sully <dsully@users.noreply.github.com>
2025-09-25 06:27:08 -0700

    chore: update claude-code-acp build

--- packages/claude-code-acp/default.nix
+++ packages/claude-code-acp/default.nix
@@ -1,40 +1,45 @@
 {pkgs, ...}:
 with pkgs;
   buildNpmPackage rec {
+    pname = "claude-code-acp";
+    version = "0.4.5";
 
-  pname = "claude-code-acp";
-  version = "0.4.4";
+    src = fetchFromGitHub {
+      owner = "zed-industries";
+      repo = "claude-code-acp";
+      rev = version;
+      hash = "sha256-kkAQuYP2S5EwIGJV8TLrlYzHOC54vmxEHwwuZD5P1hI=";
+    };
 
-  src = fetchzip {
-    url = "https://registry.npmjs.org/@zed-industries/claude-code-acp/-/claude-code-acp-${version}.tgz";
-    hash = "sha256-kkAQuYP2S5EwIGJV8TLrlYzHOC54vmxEHwwuZD5P1hI=";
-  };
+    doCheck = false;
+    dontNpmBuild = true;
+    makeCacheWritable = true;
+    npmDepsHash = "sha256-IR88NP1AiR6t/MLDdaZY1Np0AE7wfqEUfmnohaf0ymc=";
 
-  npmDepsHash = "sha256-tfsSjCATaP43dsylWRcy5KOCoO7taS88V49tRwveFjg=";
-  makeCacheWritable = true;
+    nativeBuildInputs = [
+      pkgs.patchelf
+    ];
 
-  postPatch = ''
-    cp ${./package-lock.json} package-lock.json
-  '';
+    postPatch = ''
+      cp ${./package-lock.json} package-lock.json
+    '';
 
-  dontNpmBuild = true;
+    AUTHORIZED = "1";
 
-  AUTHORIZED = "1";
+    # `claude-code` tries to auto-update by default, this disables that functionality.
+    # https://docs.anthropic.com/en/docs/agents-and-tools/claude-code/overview#environment-variables
+    postInstall = ''
+      wrapProgram $out/bin/claude-code-acp \
+        --set DISABLE_AUTOUPDATER 1
+    '';
 
-  # `claude-code` tries to auto-update by default, this disables that functionality.
-  # https://docs.anthropic.com/en/docs/agents-and-tools/claude-code/overview#environment-variables
-  postInstall = ''
-    wrapProgram $out/bin/claude-code-acp \
-      --set DISABLE_AUTOUPDATER 1
-  '';
+    passthru.updateScript = ./update.sh;
 
-  passthru.updateScript = ./update.sh;
-
-  meta = {
-    description = "An agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
-    homepage = "https://github.com/zed-industries/claude-code-acp";
-    downloadPage = "https://www.npmjs.com/package/@zed-industries/claude-code-acp";
-    license = pkgs.lib.licenses.asl20;
-    mainProgram = "claude-code-acp";
-  };
-}
+    meta = {
+      description = "An agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
+      homepage = "https://github.com/zed-industries/claude-code-acp";
+      downloadPage = "https://www.npmjs.com/package/@zed-industries/claude-code-acp";
+      license = pkgs.lib.licenses.asl20;
+      mainProgram = "claude-code-acp";
+    };
+  }
