f8e174cd07a0a6ed1d90571f807155f77731eb85
Dan Sully <dsully@users.noreply.github.com>
2025-10-10 08:57:06 -0700

    update caddy hashes

--- hosts/server/users/dsully.nix
+++ hosts/server/users/dsully.nix
@@ -24,10 +24,10 @@
           {
             plugins = [
               "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
-              # "github.com/caddy-dns/linode@b63fbb996d391e0668ce1174a2ca68ccac63201f"
+              "github.com/caddy-dns/linode@v0.8.0"
               "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
             ];
-            hash = "sha256-5HCHGxAk8pskatPMvpBiCXRMQcfjZ9o8ATfEVL7mDWE=";
+            hash = "sha256-p9ZPtN7SewWNlUHClxnnsqi8OIFqydWb3Smg/qjxEgU=";
           })
       ]
       ++ (with perSystem.self; [
