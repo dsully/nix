91d4a2118973ab53b1eaf11f4ae8a89cff132dc9
Dan Sully <dsully@users.noreply.github.com>
2025-10-21 08:09:42 -0700

    feat: add caddy for zap

--- hosts/zap/users/dsully.nix
+++ hosts/zap/users/dsully.nix
@@ -12,6 +12,17 @@
     packages = with pkgs; [
       nix-output-monitor
       pandoc
+
+      (caddy.withPlugins
+        {
+          plugins = [
+            "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
+            "github.com/caddy-dns/linode@v0.8.0"
+            "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
+            "github.com/abiosoft/caddy-json-schema@v0.0.0-20220621031927-c4d6e132f3af"
+          ];
+          hash = "sha256-cFeTDqf3Uj06fZFnf1q6c8GotgJ3dU+DrkPdQijv3cQ=";
+        })
     ];
   };
 
