12ec2001dce0da80d10038057169d55e84caaa24
Dan Sully <dsully@users.noreply.github.com>
2025-10-30 03:59:20 -0700

    chore: update lockfile and versions

--- packages/pyrefly.nix
+++ packages/pyrefly.nix
@@ -3,11 +3,11 @@ with pkgs; let
   dists = {
     aarch64-darwin = {
       platform = "macosx_11_0_arm64";
-      hash = "sha256-9WJl/qIF1snwbT7Y3xlOYxCUC04vpyrHi88+dQnwf1k=";
+      hash = "sha256-xmoU9ey5gg1IISbyD+8CMbVT/ScCvRFa0qDfeEgWW5c=";
     };
     x86_64-linux = {
       platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
-      hash = "sha256-GXhbzMVBhyFzrEHHq/ZiNkiUkGid976cnapWvcbCBtY=";
+      hash = "sha256-S/jb5i94u9DzaxuUvCF7HxI9r6nSgB9bJudlDPVqQxc=";
     };
   };
 
@@ -15,7 +15,7 @@ with pkgs; let
 in
   python3.pkgs.buildPythonPackage rec {
     pname = "pyrefly";
-    version = "0.39.2";
+    version = "0.39.3";
     format = "wheel";
 
     src = fetchPypi {
