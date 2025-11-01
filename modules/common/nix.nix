8583dde2d86c1b07b67fb5bda31e45a38444d9da
Dan Sully <dsully@users.noreply.github.com>
2025-11-01 09:32:25 -0700

    feat: add numtide cachix

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -7,6 +7,7 @@
     "https://cache.nixos.org/"
     "https://nix-community.cachix.org"
     "https://cache.lix.systems"
+    "https://numtide.cachix.org"
     "https://dsully.cachix.org"
   ];
 in {
@@ -60,6 +61,7 @@ in {
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "dsully.cachix.org-1:smJ/u8VCUmfyavfuZBNXhXhPDfryFeo+vhYT0BPEIQo="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
 
       trusted-users = [
