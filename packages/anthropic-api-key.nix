39a7df659cd03720a264d1a16c6fa62bc439d232
Dan Sully <dsully@users.noreply.github.com>
2025-11-02 16:37:53 -0800

    fix: Let's try this

--- packages/anthropic-api-key.nix
+++ packages/anthropic-api-key.nix
@@ -4,9 +4,9 @@ with pkgs;
     pname = "anthropic-api-key";
     version = "0.1.1";
 
-    src = builtins.fetchGit {
-      url = "git@github.com:dsully/anthropic-api-key.git";
-      rev = "832230dfff0f46cd39e3166b516c3868c40a9bf5";
+    src = pkgs.fetchgit {
+      url = "git+ssh://git@github.com/dsully/anthropic-api-key.git";
+      hash = "sha256-ib0RK0JkaUKPo0LU1+8XbO36kSEqGFMK+T2yPUcJg+0=";
     };
 
     cargoHash = "sha256-Aa4jppD9SS7x6xbbZmXZFJvJQeA8oRHSGc3IpDSNTMA=";
