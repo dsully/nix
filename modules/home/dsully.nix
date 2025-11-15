0e52ccabb650f94fce1f71de49624b690558e15b
Dan Sully <dsully@users.noreply.github.com>
2025-11-15 14:14:59 -0800

    feat: add editorconfig

--- modules/home/dsully.nix
+++ modules/home/dsully.nix
@@ -19,6 +19,35 @@ in {
     ./ssh.nix
   ];
 
+  editorconfig = {
+    enable = true;
+
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        trim_trailing_whitespace = true;
+        insert_final_newline = true;
+        indent_style = "space";
+      };
+      "*.{js,json,jsonc,ts,nix,yml,yaml}" = {
+        indent_size = 2;
+        max_line_length = 160;
+      };
+      "*.{py,rs}" = {
+        indent_size = 4;
+        max_line_length = 160;
+      };
+      "*.go" = {
+        indent_size = 8;
+        indent_style = "tab";
+      };
+      "Makefile" = {
+        indent_style = "tab";
+      };
+    };
+  };
+
   home = {
     # See here what bumping this value impacts:
     # https://nix-community.github.io/home-manager/release-notes.xhtml
