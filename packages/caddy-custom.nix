1fdc0f1c4be0855d648aeac7c5395369ce83e9da
Dan Sully <dsully@users.noreply.github.com>
2026-01-05 07:39:04 -0800

    chore: Update caddy hash

--- packages/caddy-custom.nix
+++ packages/caddy-custom.nix
@@ -6,5 +6,5 @@ pkgs.caddy.withPlugins {
     "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
     "github.com/abiosoft/caddy-json-schema@v0.0.0-20220621031927-c4d6e132f3af"
   ];
-  hash = "sha256-WfGQwWI5PZSr1iC9S5nbLG/96uSI4IgQ2iundd9+hHA=";
+  hash = "sha256-NWixKSkpcbP+z4Do+1jrMXgEIE9BvMpQiSP7sGp0M5Q=";
 }
