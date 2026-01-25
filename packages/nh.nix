9a9a77c83f887875529baf623b3dcf8be3f21e9e
Dan Sully <dsully@users.noreply.github.com>
2026-01-25 09:16:16 -0800

    chore: update lockfile and versions

--- packages/nh.nix
+++ packages/nh.nix
@@ -2,14 +2,14 @@
 with pkgs;
   rustPlatform.buildRustPackage rec {
     pname = "nh";
-    rev = "f3ada037c14f92cb7a56f18768e6d80219c2431f";
+    rev = "2929047421fc0cbc31c45fab001c5e1bf3e837fd";
     version = "4.3.0-beta1-${rev}";
 
     src = fetchFromGitHub {
       inherit rev;
       owner = "nix-community";
       repo = "nh";
-      hash = "sha256-ICMH2e+ZQqN+VA8Jf5haxWevwkjBlbppTmuMEN3d4eY=";
+      hash = "sha256-gCpkjnputz5etOnUob73hciZ8OXAQ8/FecXzF2P8k4Q=";
     };
 
     cargoHash = "sha256-DsDp9qTBGqzNDbyez2D95Lok7hfcmPzU4mtHJLCm0CE=";
