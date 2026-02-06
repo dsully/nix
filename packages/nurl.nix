81a9adaed4067f96b9f0b21ad718636c54d226f9
Dan Sully <dsully@users.noreply.github.com>
2026-02-05 17:48:50 -0800

    chore: update lockfile and versions

--- packages/nurl.nix
+++ packages/nurl.nix
@@ -2,17 +2,17 @@
 with pkgs;
   rustPlatform.buildRustPackage rec {
     pname = "nurl";
-    rev = "2f91e07c0a4499851f1d07806cc31114484ee0ee";
+    rev = "892e602cfa77b075f64fbcbbe2a9250e1277ab4d";
     version = "0.3.13-${rev}";
 
     src = fetchFromGitHub {
       inherit rev;
       owner = "nix-community";
       repo = "nurl";
-      hash = "sha256-1mcy36mvmCvV9FHbg/mOzj24ip6H6VsmJtq5YJGJWq0=";
+      hash = "sha256-2i3Dpc7u9NgG6jlYD6+gZnjEtWRDKsI6pL0FOwPq5iA=";
     };
 
-    cargoHash = "sha256-P3cLzZ9bcTArvxzX3As7Oo3h5Dzxkjbkw2d20EPncJo=";
+    cargoHash = "sha256-AlCWueIbkNSj85sL0nnW/O+pTuNqqHjHXF5lNOpvmzg=";
     doCheck = false;
 
     meta = {
