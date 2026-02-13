72cd69cba7ca1e41e8f175718b21f8ed41b573a0
Dan Sully <dsully@users.noreply.github.com>
2026-02-13 15:29:55 -0800

    chore: update lockfile and versions

--- packages/worktrunk.nix
+++ packages/worktrunk.nix
@@ -2,14 +2,14 @@
 with pkgs;
   rustPlatform.buildRustPackage rec {
     pname = "worktrunk";
-    version = "0.23.2";
+    version = "0.23.3";
 
     src = fetchCrate {
       inherit pname version;
-      hash = "sha256-ohJuu9y4ibXn3tkPJOyN3v3Ml8QWkYLBS5mYWvwxSII=";
+      hash = "sha256-4nitPXuSvuTksQGCrkkPTQZDLm87gbPLRxgB95nVCVw=";
     };
 
-    cargoHash = "sha256-/qmX+b9aU84ipNL9noQ0xtXyxshd07hAgYGPBBx5mwg=";
+    cargoHash = "sha256-rntixZyiSe6MXj0v/VZ3NS2jFrHbpPyZ9RP8sSdtPz8=";
     doCheck = false;
 
     meta = {
