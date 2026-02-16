df9b525bbd5bc97d7ff70f55d09acf680c86b759
Dan Sully <dsully@users.noreply.github.com>
2026-02-16 09:32:47 -0800

    fix(neovim): emmylua_rust_analyzer looks in ~/

--- modules/home/configs/emmylua.nix
+++ modules/home/configs/emmylua.nix
@@ -1,5 +1,4 @@
 {
-  # config,
   inputs,
   pkgs,
   ...
@@ -7,7 +6,7 @@
   jsonFormat = pkgs.formats.json {};
   inherit (inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}) neovim;
 in {
-  xdg.configFile."emmyrc.json".source = jsonFormat.generate "emmyrc.json" {
+  home.file.".emmyrc.json".source = jsonFormat.generate "emmyrc.json" {
     "$schema" = "https://raw.githubusercontent.com/EmmyLuaLs/emmylua-analyzer-rust/refs/heads/main/crates/emmylua_code_analysis/resources/schema.json";
     diagnostics = {
       disable = [
