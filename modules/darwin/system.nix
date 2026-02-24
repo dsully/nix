9310c2507ea7925af28a57f6a418dbca4be218ab
Dan Sully <dsully@users.noreply.github.com>
2026-02-24 08:56:27 -0800

    chore: Update to be launchd

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,6 +1,27 @@
 {lib, ...}: {
   power.sleep.computer = 15;
 
+  # TCP performance tuning via LaunchDaemon (persists across reboots)
+  # https://rolande.wordpress.com/2025/08/07/performance-tuning-the-network-stack-on-macos-sequoia-15-6/
+  launchd.daemons.sysctl-tcp-tuning = {
+    serviceConfig = {
+      ProgramArguments = [
+        "/usr/sbin/sysctl"
+        "-w"
+        "net.inet.tcp.delayed_ack=0"
+        "net.inet.tcp.mssdflt=1360"
+        "net.inet.tcp.blackhole=2"
+        "net.inet.tcp.win_scale_factor=7"
+        "net.inet.tcp.sendspace=861275"
+        "net.inet.tcp.recvspace=861275"
+        "net.inet.tcp.autorcvbufmax=8388608"
+        "net.inet.tcp.autosndbufmax=8388608"
+      ];
+      RunAtLoad = true;
+      LaunchOnlyOnce = true;
+    };
+  };
+
   system.activationScripts.postActivation.text = lib.mkAfter ''
     launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist 2>/dev/null || true
     launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist 2>/dev/null || true
@@ -16,19 +37,5 @@
 
     # Disable Time Machine
     /usr/bin/tmutil disable 2>/dev/null || true
-
-    # TCP performance tuning
-    # https://rolande.wordpress.com/2025/08/07/performance-tuning-the-network-stack-on-macos-sequoia-15-6/
-    #
-    sysctl -w net.inet.tcp.delayed_ack=0 2>/dev/null || true
-    sysctl -w net.inet.tcp.mssdflt=1360 2>/dev/null || true
-    sysctl -w net.inet.tcp.blackhole=2 2>/dev/null || true
-    sysctl -w net.inet.tcp.win_scale_factor=7 2>/dev/null || true
-
-    sysctl -w net.inet.tcp.sendspace=861275 2>/dev/null || true
-    sysctl -w net.inet.tcp.recvspace=861275 2>/dev/null || true
-
-    sysctl -w net.inet.tcp.autorcvbufmax=8388608 2>/dev/null || true
-    sysctl -w net.inet.tcp.autosndbufmax=8388608 2>/dev/null || true
   '';
 }
