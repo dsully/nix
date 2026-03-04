dc0ff80bdf3388c111796071deabf54390395caf
Dan Sully <dsully@users.noreply.github.com>
2026-03-04 08:31:31 -0800

    chore: home-manager reverted the nix profile change

--- modules/home/dsully.nix
+++ modules/home/dsully.nix
@@ -75,17 +75,6 @@ in {
     stateVersion = "25.05";
 
     activation = {
-      #
-      # https://github.com/nix-community/home-manager/issues/8786#issuecomment-3964343590
-      installPackages = lib.mkForce (lib.hm.dag.entryAfter ["writeBoundary"] ''
-        nixProfileRemove home-manager-path
-        if [[ -e ${config.home.profileDirectory}/manifest.json ]]; then
-          run nix profile install ${config.home.path}
-        else
-          run nix-env -i ${config.home.path}
-        fi
-      '');
-
       neovim = inputs.home-manager.lib.hm.dag.entryAfter ["writeBoundary" "installPackages"] ''
         #!/bin/bash
 
