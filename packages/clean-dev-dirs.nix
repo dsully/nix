5752deb2664b5cb7671ca655f164d9491141c5ac
Dan Sully <dsully@users.noreply.github.com>
2026-03-23 17:34:29 -0700

    chore: update lockfile and versions

--- packages/clean-dev-dirs.nix
+++ packages/clean-dev-dirs.nix
@@ -5,14 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "clean-dev-dirs";
-  version = "2.7.1";
+  version = "2.8.0";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-64BWCT7kC9BqM14LeJtkwB2dzg5TKJPjgsUPdr1wr8c=";
+    hash = "sha256-E26t+vItr4OSH4jJ8FohQqaQZ+v9CJ91w7TkAu41tyw=";
   };
 
-  cargoHash = "sha256-Wa0sYRG1ifb01sFeM8wrMPr08+pzocLOfA6PRyVwhc8=";
+  cargoHash = "sha256-MEdA1qb7fqQnspvZe+kimBuvBxK/Wf5ZtDR+FPrIOPY=";
   doCheck = false;
 
   meta = {
