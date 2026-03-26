97b1266b3453f56489077924004f061cca0e5614
Dan Sully <dsully@users.noreply.github.com>
2026-03-26 12:35:58 -0700

    fix: Update fish venv script to handle nested venvs

--- dotfiles/fish/functions/__python_virtualenv.fish
+++ dotfiles/fish/functions/__python_virtualenv.fish
@@ -1,40 +1,31 @@
 function __python_virtualenv --description "Auto activate/deactivate Python virtualenvs"
 
-    # Check if we are inside a git repository
-    if git rev-parse --show-toplevel &>/dev/null
-        set root (realpath (git rev-parse --show-toplevel 2>/dev/null))
-    else
-        set root (pwd -P)
-    end
-
     set -l venv_path ""
+    set -l cwd (pwd -P)
 
-    # Case #1: cd'd into a folder which has a .venv in its root.
-    #
-    # Activate this virtualenv if it's not already activated.
-    if test -e "$root/.venv/bin/activate.fish"
-        set venv_path "$root/.venv"
-    end
+    # Check current directory first (nearest .venv wins), then git root as fallback.
+    if test -e "$cwd/.venv/bin/activate.fish"
+        set venv_path "$cwd/.venv"
 
-    if test -n "$venv_path" -a "$VIRTUAL_ENV" != "$venv_path" -a -e "$venv_path/bin/activate.fish"
+    else if git rev-parse --show-toplevel &>/dev/null
+        set -l root (realpath (git rev-parse --show-toplevel 2>/dev/null))
 
-        source "$root/.venv/bin/activate.fish" &>/dev/null
-
-        # set -gx VIRTUAL_ENV $venv_path
-        # set -gx PATH "$VIRTUAL_ENV/bin" $PATH
-
-    else if test -n "$VIRTUAL_ENV" -a -z "$venv_path"
-
-        if functions -q deactivate
-            deactivate
-            # else
-            #     # Manual cleanup if deactivate isn't available
-            #     set -e VIRTUAL_ENV
+        if test -e "$root/.venv/bin/activate.fish"
+            set venv_path "$root/.venv"
         end
     end
 
-    # If there's a virtualenv in the root of this repo, we should activate it now.
-    # if test -d "$root/.venv"
-    #     source "$root/.venv/bin/activate.fish" &>/dev/null
-    # end
+    if test -n "$venv_path" -a "$VIRTUAL_ENV" != "$venv_path"
+        # Deactivate any existing venv before activating the new one.
+        if test -n "$VIRTUAL_ENV"; and functions -q deactivate
+            deactivate
+        end
+
+        source "$venv_path/bin/activate.fish" &>/dev/null
+
+    else if test -z "$venv_path" -a -n "$VIRTUAL_ENV"
+        if functions -q deactivate
+            deactivate
+        end
+    end
 end
