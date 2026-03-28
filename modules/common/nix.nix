3123648bd961969b0788859c83315c088900436d
Dan Sully <dsully@users.noreply.github.com>
2026-03-28 09:52:28 -0700

    chore: Bump versions & remove direnv override

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -117,20 +117,5 @@ in {
       };
 
     nixpkgs.config.allowUnfree = true;
-
-    # https://github.com/NixOS/nixpkgs/issues/502464
-    nixpkgs.overlays = [
-      (_: prev: {
-        direnv = prev.direnv.overrideAttrs (old: {
-          allowGoReference = true;
-          env.CGO_ENABLED = 0;
-          postPatch =
-            (old.postPatch or "")
-            + ''
-              substituteInPlace GNUmakefile --replace-warn '-linkmode=external' ""
-            '';
-        });
-      })
-    ];
   };
 }
