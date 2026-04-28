4aedec99f8808cbab8a9afc88d3638051802c7c4
Dan Sully <dsully@users.noreply.github.com>
2026-04-28 07:40:51 -0700

    chore: update lockfile and versions

--- modules/home/configs/emmylua.nix
+++ modules/home/configs/emmylua.nix
@@ -4,52 +4,57 @@
   ...
 }: let
   jsonFormat = pkgs.formats.json {};
+  emmyrc = extraLibraries:
+    jsonFormat.generate "emmyrc.json" {
+      "$schema" = "https://raw.githubusercontent.com/EmmyLuaLs/emmylua-analyzer-rust/refs/heads/main/crates/emmylua_code_analysis/resources/schema.json";
+      diagnostics = {
+        disable = [
+          "missing-fields"
+          "type-not-found"
+          "undefined-field"
+          "unnecessary-if"
+        ];
+        globals = [
+          "Snacks"
+          "bit"
+          "colors"
+          "defaults"
+          "ev"
+          "hl"
+          "keys"
+          "ns"
+          "nvim"
+          "package"
+          "require"
+          "vim"
+        ];
+        unusedLocalExclude = [
+          "_*"
+        ];
+      };
+      runtime = {
+        version = "LuaJIT";
+      };
+      workspace = {
+        ignoreDir = [
+          "debug"
+          "templates"
+        ];
+        ignoreGlobs = [
+          "**/*_spec.lua"
+        ];
+        library =
+          [
+            "$VIMRUNTIME"
+          ]
+          ++ extraLibraries;
+      };
+    };
 in {
-  home.file.".emmyrc.json".source = jsonFormat.generate "emmyrc.json" {
-    "$schema" = "https://raw.githubusercontent.com/EmmyLuaLs/emmylua-analyzer-rust/refs/heads/main/crates/emmylua_code_analysis/resources/schema.json";
-    diagnostics = {
-      disable = [
-        "missing-fields"
-        "type-not-found"
-        "undefined-field"
-        "unnecessary-if"
-      ];
-      globals = [
-        "Snacks"
-        "bit"
-        "colors"
-        "defaults"
-        "ev"
-        "hl"
-        "keys"
-        "ns"
-        "nvim"
-        "package"
-        "require"
-        "vim"
-      ];
-      unusedLocalExclude = [
-        "_*"
-      ];
-    };
-    runtime = {
-      version = "LuaJIT";
-    };
-    workspace = {
-      ignoreDir = [
-        "debug"
-        "templates"
-      ];
-      ignoreGlobs = [
-        "**/*_spec.lua"
-      ];
-      library = [
-        "$VIMRUNTIME"
-        "${config.xdg.dataHome}/nvim/lazy"
-        "${config.xdg.dataHome}/nvim/site/pack/core/opt"
-      ];
-    };
-  };
+  home.file.".emmyrc.json".source = emmyrc [];
 
-  xdg.configFile."nvim/.emmyrc.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.emmyrc.json";
+  home.file."${config.xdg.configHome}/nvim/.emmyrc.json".source = emmyrc [
+    "${config.xdg.dataHome}/nvim/lazy"
+    "${config.xdg.dataHome}/nvim/site/pack/core/opt"
+  ];
 }
