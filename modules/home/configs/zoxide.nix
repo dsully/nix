f4ede5dced7065c5144e2477d188a9a4c91d44f2
Dan Sully <dsully@users.noreply.github.com>
2026-05-02 10:31:32 -0700

    feat: Add prune-zoxide fish function

--- modules/home/configs/zoxide.nix
+++ modules/home/configs/zoxide.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   fzfCfg = config.programs.fzf;
@@ -15,11 +16,26 @@ in {
     _ZO_FZF_OPTS = "${fzfOpts} --bind=ctrl-z:ignore --exit-0 --info=default --layout=reverse-list --no-sort --select-1";
   };
 
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-    options = [
-      "--cmd j"
-    ];
+  programs = {
+    fish.functions.prune-zoxide = {
+      description = "Remove paths that are no longer on disk in zoxide";
+      body =
+        # fish
+        ''
+          ${lib.getExe pkgs.zoxide} query --list --all | while read -l path
+              if not test -e "$path"
+                  ${lib.getExe pkgs.zoxide} remove "$path"
+              end
+          end
+        '';
+    };
+
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+      options = [
+        "--cmd j"
+      ];
+    };
   };
 }
