21c6d5d46221440945985158d9782caeca3629a4
Dan Sully <dsully@users.noreply.github.com>
2026-05-02 15:11:21 -0700

    chore: Only build caddy on Linux

--- packages/caddy-custom.nix
+++ packages/caddy-custom.nix
@@ -1,12 +1,20 @@
-{caddy}:
-(caddy.withPlugins {
-  plugins = [
-    "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
-    "github.com/caddy-dns/linode@v0.8.0"
-    "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
-    "github.com/abiosoft/caddy-json-schema@v0.0.0-20220621031927-c4d6e132f3af"
-  ];
-  hash = "sha256-xEhCgcKSoVRke4mNgOMkdlGbf4pLuLCBWeN7lxxhOZA=";
-}).overrideAttrs {
-  doCheck = false;
-}
+{
+  caddy,
+  emptyFile,
+  stdenv,
+  ...
+}:
+if stdenv.hostPlatform.isLinux
+then
+  (caddy.withPlugins {
+    plugins = [
+      "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
+      "github.com/caddy-dns/linode@v0.8.0"
+      "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
+      "github.com/abiosoft/caddy-json-schema@v0.0.0-20220621031927-c4d6e132f3af"
+    ];
+    hash = "sha256-xEhCgcKSoVRke4mNgOMkdlGbf4pLuLCBWeN7lxxhOZA=";
+  }).overrideAttrs {
+    doCheck = false;
+  }
+else emptyFile
