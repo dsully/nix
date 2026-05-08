eccff5dacb13b1c884098c5f5ccf46fd7cd4182b
Dan Sully <dsully@users.noreply.github.com>
2026-05-08 07:12:13 -0700

    chore: update lockfile and versions

--- packages/rust-markdown-lsp-server.nix
+++ packages/rust-markdown-lsp-server.nix
@@ -5,14 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "rust-markdown-lsp-server";
-  rev = "cd72052eba2c81b5f9a0a1b6da01b11ef3b0db00";
+  rev = "9ac5212981ebfe51db45c855ab2fd360e1db03ce";
   version = "0.1.0-${rev}";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "dougrocha";
     repo = "rust_markdown_lsp_server";
-    hash = "sha256-rhzik5NBw89jEBLkDqJyMsgdN2A/M0J0P4Ml2Z+SXWw=";
+    hash = "sha256-WM0hfDJ1/c0peB1GRcGJC+zIrX5nafmPedqgQYmXxok=";
   };
 
   cargoHash = "sha256-VHJ9dz6MWBWRU75Cx1vWjZvtKMdfTTStr8yS+eZR/cY=";
