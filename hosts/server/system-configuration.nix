349738106675d9313c2ba87eb8f8eec37d7dfa00
Dan Sully <dsully@users.noreply.github.com>
2026-05-10 15:36:11 -0700

    style(server): add syntax hints and normalize multiline string indentation
    - Add language hint comments (`# bash`, `# caddy`, `# yaml`, `# ini`) to multiline string literals for editor syntax highlighting
    - Re-indent embedded config content to align with Nix multiline string formatting conventions

--- hosts/server/system-configuration.nix
+++ hosts/server/system-configuration.nix
@@ -19,293 +19,305 @@ in {
   config = {
     environment = {
       etc = {
-        "sudoers.d/10-nix-commands".text = ''
-          Cmnd_Alias SYSTEM_MANAGER = ${system-manager}/bin/system-manager (build|switch) *
-          Cmnd_Alias LIQUIDCTL = ${lib.getExe pkgs.liquidctl} (list|status)
-          Cmnd_Alias NIX = ${lib.getExe pkgs.lix} *
-          Cmnd_Alias VPN_CLEANUP = /sbin/ip link delete vpn_s, /sbin/ip link delete vpn_d, /sbin/ip netns delete vpn
+        "sudoers.d/10-nix-commands".text =
+          # bash
+          ''
+            Cmnd_Alias SYSTEM_MANAGER = ${system-manager}/bin/system-manager (build|switch) *
+            Cmnd_Alias LIQUIDCTL = ${lib.getExe pkgs.liquidctl} (list|status)
+            Cmnd_Alias NIX = ${lib.getExe pkgs.lix} *
+            Cmnd_Alias VPN_CLEANUP = /sbin/ip link delete vpn_s, /sbin/ip link delete vpn_d, /sbin/ip netns delete vpn
 
-          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:LIQUIDCTL
-          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:NIX
-          %admin ALL=(ALL:ALL) NOPASSWD:SETENV:SYSTEM_MANAGER
-          %admin ALL=(ALL:ALL) NOPASSWD:VPN_CLEANUP
+            %admin ALL=(ALL:ALL) NOPASSWD:SETENV:LIQUIDCTL
+            %admin ALL=(ALL:ALL) NOPASSWD:SETENV:NIX
+            %admin ALL=(ALL:ALL) NOPASSWD:SETENV:SYSTEM_MANAGER
+            %admin ALL=(ALL:ALL) NOPASSWD:VPN_CLEANUP
 
-          Defaults:%sudo env_keep += "PATH SSH_AGENT_PID SSH_AUTH_SOCK TERM TERMINFO"
-        '';
+            Defaults:%sudo env_keep += "PATH SSH_AGENT_PID SSH_AUTH_SOCK TERM TERMINFO"
+          '';
 
-        "caddy/Caddyfile".text = ''
-          {
-            default_sni server.sully.org
-            http_port 80
-            https_port 443
+        "caddy/Caddyfile".text =
+          # caddy
+          ''
+            {
+              default_sni server.sully.org
+              http_port 80
+              https_port 443
 
-            servers {
-              protocols h1 h2
-            }
-          }
-
-          *.sully.org {
-            encode zstd gzip
-
-            log {
-              output file ${caddyLogDir}/access.log {
-                roll_size 64mb
-                roll_keep 5
-                roll_keep_for 720h
+              servers {
+                protocols h1 h2
               }
             }
 
-            tls {
-              dns cloudflare {env.CLOUDFLARE_API_TOKEN}
-            }
+            *.sully.org {
+              encode zstd gzip
 
-            @syncthing host syncthing.sully.org
-            handle @syncthing {
-              reverse_proxy https://127.0.0.1:8384 {
-                header_up Host syncthing.sully.org
-                header_up X-Forwarded-Proto https
-                transport http {
-                  tls
-                  tls_insecure_skip_verify
+              log {
+                output file ${caddyLogDir}/access.log {
+                  roll_size 64mb
+                  roll_keep 5
+                  roll_keep_for 720h
                 }
               }
-            }
 
-            @plex host plex.sully.org
-            handle @plex {
-              reverse_proxy 127.0.0.1:32400
-            }
-
-            @dl host dl.sully.org
-            handle @dl {
-              reverse_proxy http://localhost:9091 {
-                header_up Host 127.0.0.1:9091
-                header_up -Origin
-                header_up -Referer
-                header_down -content-security-policy
-                header_down -x-frame-options
-                header_down -x-webkit-csp
-                header_down -x-xss-protection
+              tls {
+                dns cloudflare {env.CLOUDFLARE_API_TOKEN}
               }
-            }
 
-            @overseerr host overseerr.sully.org
-            handle @overseerr {
-              reverse_proxy 127.0.0.1:5055
-            }
-
-            @homebridge host homebridge.sully.org
-            handle @homebridge {
-              reverse_proxy 127.0.0.1:8581
-            }
-
-            @resilio host resilio.sully.org
-            handle @resilio {
-              reverse_proxy 127.0.0.1:8888 {
-                transport http {
-                  tls
-                  tls_insecure_skip_verify
+              @syncthing host syncthing.sully.org
+              handle @syncthing {
+                reverse_proxy https://127.0.0.1:8384 {
+                  header_up Host syncthing.sully.org
+                  header_up X-Forwarded-Proto https
+                  transport http {
+                    tls
+                    tls_insecure_skip_verify
+                  }
                 }
               }
-            }
 
-            @radarr host radarr.sully.org
-            handle @radarr {
-              reverse_proxy 127.0.0.1:7878
-            }
+              @plex host plex.sully.org
+              handle @plex {
+                reverse_proxy 127.0.0.1:32400
+              }
 
-            @sonarr host sonarr.sully.org
-            handle @sonarr {
-              reverse_proxy 127.0.0.1:8989
-            }
-
-            @prowlarr host prowlarr.sully.org
-            handle @prowlarr {
-              reverse_proxy 127.0.0.1:9696
-            }
-
-            @subtitles host subtitles.sully.org
-            handle @subtitles {
-              reverse_proxy 127.0.0.1:6767
-            }
-
-            @stash host stash.sully.org
-            handle @stash {
-              reverse_proxy 127.0.0.1:9999
-            }
-
-            @scrypted host scrypted.sully.org
-            handle @scrypted {
-              reverse_proxy https://127.0.0.1:10443 {
-                header_up Host scrypted.sully.org
-                header_up X-Forwarded-Proto https
-                transport http {
-                  tls
-                  tls_insecure_skip_verify
+              @dl host dl.sully.org
+              handle @dl {
+                reverse_proxy http://localhost:9091 {
+                  header_up Host 127.0.0.1:9091
+                  header_up -Origin
+                  header_up -Referer
+                  header_down -content-security-policy
+                  header_down -x-frame-options
+                  header_down -x-webkit-csp
+                  header_down -x-xss-protection
                 }
               }
-            }
 
-            @breadboard host breadboard.sully.org
-            handle @breadboard {
-              reverse_proxy 127.0.0.1:4200
-            }
+              @overseerr host overseerr.sully.org
+              handle @overseerr {
+                reverse_proxy 127.0.0.1:5055
+              }
 
-            @captions host captions.sully.org
-            handle @captions {
-              reverse_proxy 127.0.0.1:1337
-            }
+              @homebridge host homebridge.sully.org
+              handle @homebridge {
+                reverse_proxy 127.0.0.1:8581
+              }
 
-            @comfy host comfy.sully.org
-            handle @comfy {
-              reverse_proxy 127.0.0.1:8188
-            }
+              @resilio host resilio.sully.org
+              handle @resilio {
+                reverse_proxy 127.0.0.1:8888 {
+                  transport http {
+                    tls
+                    tls_insecure_skip_verify
+                  }
+                }
+              }
 
-            @invoke host invoke.sully.org
-            handle @invoke {
-              reverse_proxy 127.0.0.1:9090
-            }
+              @radarr host radarr.sully.org
+              handle @radarr {
+                reverse_proxy 127.0.0.1:7878
+              }
 
-            @pose host pose.sully.org
-            handle @pose {
-              reverse_proxy 127.0.0.1:5173
-            }
+              @sonarr host sonarr.sully.org
+              handle @sonarr {
+                reverse_proxy 127.0.0.1:8989
+              }
 
-            @sd host sd.sully.org
-            handle @sd {
-              reverse_proxy 127.0.0.1:7860
-            }
+              @prowlarr host prowlarr.sully.org
+              handle @prowlarr {
+                reverse_proxy 127.0.0.1:9696
+              }
 
-            @train host train.sully.org
-            handle @train {
-              reverse_proxy 127.0.0.1:7861
+              @subtitles host subtitles.sully.org
+              handle @subtitles {
+                reverse_proxy 127.0.0.1:6767
+              }
+
+              @stash host stash.sully.org
+              handle @stash {
+                reverse_proxy 127.0.0.1:9999
+              }
+
+              @scrypted host scrypted.sully.org
+              handle @scrypted {
+                reverse_proxy https://127.0.0.1:10443 {
+                  header_up Host scrypted.sully.org
+                  header_up X-Forwarded-Proto https
+                  transport http {
+                    tls
+                    tls_insecure_skip_verify
+                  }
+                }
+              }
+
+              @breadboard host breadboard.sully.org
+              handle @breadboard {
+                reverse_proxy 127.0.0.1:4200
+              }
+
+              @captions host captions.sully.org
+              handle @captions {
+                reverse_proxy 127.0.0.1:1337
+              }
+
+              @comfy host comfy.sully.org
+              handle @comfy {
+                reverse_proxy 127.0.0.1:8188
+              }
+
+              @invoke host invoke.sully.org
+              handle @invoke {
+                reverse_proxy 127.0.0.1:9090
+              }
+
+              @pose host pose.sully.org
+              handle @pose {
+                reverse_proxy 127.0.0.1:5173
+              }
+
+              @sd host sd.sully.org
+              handle @sd {
+                reverse_proxy 127.0.0.1:7860
+              }
+
+              @train host train.sully.org
+              handle @train {
+                reverse_proxy 127.0.0.1:7861
+              }
             }
-          }
-        '';
+          '';
 
         "doas.conf" = {
-          text = ''
-            permit root
-            permit keepenv persist dsully
+          text =
+            # bash
+            ''
+              permit root
+              permit keepenv persist dsully
 
-            permit nopass dsully as root cmd apt-get args clean
-            permit nopass dsully as root cmd apt-get args update
-            permit nopass dsully as root cmd apt-get args dist-upgrade -y
-            permit nopass dsully as root cmd apt-get args autoremove -y
-            permit nopass dsully as root cmd /usr/bin/snap args refresh
-          '';
+              permit nopass dsully as root cmd apt-get args clean
+              permit nopass dsully as root cmd apt-get args update
+              permit nopass dsully as root cmd apt-get args dist-upgrade -y
+              permit nopass dsully as root cmd apt-get args autoremove -y
+              permit nopass dsully as root cmd /usr/bin/snap args refresh
+            '';
           mode = "0400";
         };
 
-        "netplan/01-netcfg.yaml".text = ''
-          network:
-            version: 2
-            renderer: "networkd"
-            ethernets:
-              eth0:
-                accept-ra: true
-                addresses: ["10.0.0.100/24"]
-                dhcp4: false
-                dhcp6: false
-                match:
-                  macaddress: 3c:8c:f8:60:18:c1
-                optional: false
-                routes:
-                  - to: default
-                    via: 10.0.0.1
-                    metric: 100
-                    on-link: true
-                set-name: eth0
-        '';
+        "netplan/01-netcfg.yaml".text =
+          # yaml
+          ''
+            network:
+              version: 2
+              renderer: "networkd"
+              ethernets:
+                eth0:
+                  accept-ra: true
+                  addresses: ["10.0.0.100/24"]
+                  dhcp4: false
+                  dhcp6: false
+                  match:
+                    macaddress: 3c:8c:f8:60:18:c1
+                  optional: false
+                  routes:
+                    - to: default
+                      via: 10.0.0.1
+                      metric: 100
+                      on-link: true
+                  set-name: eth0
+          '';
 
-        "rsyncd.conf".text = ''
-          uid = root
-          gid = root
-          use chroot = no
-          syslog facility = user
+        "rsyncd.conf".text =
+          # ini
+          ''
+            uid = root
+            gid = root
+            use chroot = no
+            syslog facility = user
 
-          [bits]
-            comment = bits
-            dont compress = *.mp3 *.MP3 *.zip *.ZIP *.gz *.bz2 *.dmg *.iso *.mkv *.mp4 *.flac *.aac *.wmv
-            hosts allow jarvis
-            path = /bits/
-            read only = yes
-            reverse lookup yes
+            [bits]
+              comment = bits
+              dont compress = *.mp3 *.MP3 *.zip *.ZIP *.gz *.bz2 *.dmg *.iso *.mkv *.mp4 *.flac *.aac *.wmv
+              hosts allow jarvis
+              path = /bits/
+              read only = yes
+              reverse lookup yes
 
-          [models]
-            dont compress = *.gz *.tgz *.zip *.z *.rpm *.deb *.iso *.bz2 *.tbz *.safetensors *.ckpt
-            hosts allow jarvis
-            list = no
-            path = /ai/models/
+            [models]
+              dont compress = *.gz *.tgz *.zip *.z *.rpm *.deb *.iso *.bz2 *.tbz *.safetensors *.ckpt
+              hosts allow jarvis
+              list = no
+              path = /ai/models/
+              read only = no
+              reverse lookup yes
+          '';
+
+        "samba/smb.conf".text =
+          # ini
+          ''
+            [global]
+            workgroup = WORKGROUP
+            server string = %h
+            multicast dns register = no
+
+            case sensitive = true
+
+            vfs objects = fruit streams_xattr dirsort aio_pthread
+
+            ea support = yes
+            fruit:aapl = yes
+            fruit:encoding = native
+            fruit:locking = none
+            fruit:model = RackMac
+            fruit:metadata = stream
+            fruit:posix_rename = yes
+            fruit:veto_appledouble = no
+            fruit:wipe_intentionally_left_blank_rfork = yes
+            fruit:delete_empty_adfiles = yes
+            fruit:nfs_aces = no
+
+            aio_pthread:aio open = yes
+
+            dns proxy = no
+            name resolve order = host
+            preferred master = no
+            unix extensions = no
+            domain master = no
+            server signing = no
+            strict locking = no
+            strict sync = no
+            use sendfile = yes
+            aio read size = 1
+            aio write size = 1
+
+            panic action = /usr/share/samba/panic-action %d
+
+            server role = standalone server
+            passdb backend = tdbsam
+            obey pam restrictions = yes
+            unix password sync = yes
+            passwd program = /usr/bin/passwd %u
+            passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+            pam password change = yes
+            map to guest = bad user
+            inherit permissions = yes
+
+            load printers = no
+            printing = bsd
+            printcap name = /dev/null
+            disable spoolss = yes
+
+            unix extensions = no
+
+            [ai]
+            path = /ai
+            comment = AI
             read only = no
-            reverse lookup yes
-        '';
 
-        "samba/smb.conf".text = ''
-          [global]
-          workgroup = WORKGROUP
-          server string = %h
-          multicast dns register = no
-
-          case sensitive = true
-
-          vfs objects = fruit streams_xattr dirsort aio_pthread
-
-          ea support = yes
-          fruit:aapl = yes
-          fruit:encoding = native
-          fruit:locking = none
-          fruit:model = RackMac
-          fruit:metadata = stream
-          fruit:posix_rename = yes
-          fruit:veto_appledouble = no
-          fruit:wipe_intentionally_left_blank_rfork = yes
-          fruit:delete_empty_adfiles = yes
-          fruit:nfs_aces = no
-
-          aio_pthread:aio open = yes
-
-          dns proxy = no
-          name resolve order = host
-          preferred master = no
-          unix extensions = no
-          domain master = no
-          server signing = no
-          strict locking = no
-          strict sync = no
-          use sendfile = yes
-          aio read size = 1
-          aio write size = 1
-
-          panic action = /usr/share/samba/panic-action %d
-
-          server role = standalone server
-          passdb backend = tdbsam
-          obey pam restrictions = yes
-          unix password sync = yes
-          passwd program = /usr/bin/passwd %u
-          passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-          pam password change = yes
-          map to guest = bad user
-          inherit permissions = yes
-
-          load printers = no
-          printing = bsd
-          printcap name = /dev/null
-          disable spoolss = yes
-
-          unix extensions = no
-
-          [ai]
-          path = /ai
-          comment = AI
-          read only = no
-
-          [bits]
-          path = /bits
-          comment = Bits
-          read only = no
-        '';
+            [bits]
+            path = /bits
+            comment = Bits
+            read only = no
+          '';
       };
 
       systemPackages =
