c33209b70c9c05d5f0b8cb220a4f71bcf352f1a4
Dan Sully <dsully@users.noreply.github.com>
2026-05-11 08:07:35 -0700

    chore: update lockfile and versions

--- packages/icm.nix
+++ packages/icm.nix
@@ -6,11 +6,11 @@
   packages = {
     aarch64-darwin = {
       suffix = "aarch64-apple-darwin";
-      hash = "sha256-Mi3DlVIsR8PDrIlDZfX6NPDUcHIxzn/S9MoRrpi2+jM=";
+      hash = "sha256-3KGf1czeyZkLqZXBYxbs4LvJ50jC5BCwAqPSK7ymJ6Q=";
     };
     x86_64-linux = {
       suffix = "x86_64-unknown-linux-gnu";
-      hash = "sha256-4t5Pm2VvMPsMKTlMU1pLi2xz+GpbyFODwrZ4lcD7iO8=";
+      hash = "sha256-EE0NMRX3LaqScTUtWkcCS2jnMiIJeY9YrQ+44UMIj54=";
     };
   };
   source =
@@ -19,7 +19,7 @@
 in
   pkgs.stdenv.mkDerivation rec {
     pname = "icm";
-    version = "0.10.47";
+    version = "0.10.49";
 
     src = pkgs.fetchurl {
       url = "https://github.com/rtk-ai/${pname}/releases/download/${pname}-v${version}/${pname}-${source.suffix}.tar.gz";
--- packages/rust-markdown-lsp-server.nix
+++ packages/rust-markdown-lsp-server.nix
@@ -5,14 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "rust-markdown-lsp-server";
-  rev = "8ccca53c8999fa3100cbfc029c4ed2c52020cc15";
+  rev = "bf69c4bba4a7fd2e828f35e494a667c032b6ab05";
   version = "0.1.0-${rev}";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "dougrocha";
     repo = "rust_markdown_lsp_server";
-    hash = "sha256-nyN984B1s+i8v0faBZpNF0UY0EGkj5H5c7g2kQqBAmM=";
+    hash = "sha256-UR1D6+hlzxI5n5JKhmBGYHbsusDo44G1JDpUrskMFRk=";
   };
 
   cargoHash = "sha256-VHJ9dz6MWBWRU75Cx1vWjZvtKMdfTTStr8yS+eZR/cY=";
