a2c68e5178909e549dc1baf1b460917f7b213e3d
Dan Sully <dsully@users.noreply.github.com>
2026-05-26 12:51:31 -0700

    fix(fish): tower function to use the correct env variables

--- dotfiles/fish/functions/tower.fish
+++ dotfiles/fish/functions/tower.fish
@@ -2,18 +2,18 @@ function tower -d "Open Tower for directory (default: Git root)"
     set -l paths (fallback $argv (git rev-parse --show-toplevel))
 
     if test (uname) = Linux
-        if not set -q SSH_CLIENT_HOME
-            echo "SSH_CLIENT_HOME is not set"
+        if not set -q SSH_CLIENT_MOUNT
+            echo "SSH_CLIENT_MOUNT is not set"
             return 1
         end
 
         set -l remotePath (string join " " -- $paths)
 
         if string match -q "/home*" $remotePath
-            set remotePath "$SSH_CLIENT_HOME/$SSH_CLIENT_MOUNT$remotePath"
+            set remotePath "$SSH_CLIENT_MOUNT$remotePath"
         end
 
-        printf "%s" $remotePath | nc -q1 localhost 2226
+        printf "%s" "-a Tower $remotePath" | nc -q1 localhost 2226
         return
     end
 
