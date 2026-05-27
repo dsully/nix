001c177f8a9b3bb886bbaefc4e0d593fcb82266c
Dan Sully <dsully@users.noreply.github.com>
2026-05-26 18:51:40 -0700

    refactor(fish): fix cdr quoting and add Groot ASCII art
    - Quote `$cd_up_path` in test to prevent empty-string expansion bug
    - Display colored Groot ASCII art after changing to repo root

--- dotfiles/fish/functions/cdr.fish
+++ dotfiles/fish/functions/cdr.fish
@@ -4,7 +4,19 @@ function cdr --description "chdir to the root of a git repo."
 
     set -l cd_up_path (command git rev-parse --show-cdup)
 
-    if test -n $cd_up_path
+    if test -n "$cd_up_path"
         cd $cd_up_path
+
+        echo -e ""
+        echo -e "      \e[1m\e[38;5;112m\^V//"
+        echo -e "      \e[38;5;184m|\e[37m· ·\e[38;5;184m|      \e[94mI AM GROOT !"
+        echo -e "    \e[38;5;112m- \e[38;5;184m\ - /"
+        echo -e "     \_| |_/\e[38;5;112m¯"
+        echo -e "       \e[38;5;184m\ \\"
+        echo -e "     \e[38;5;124m__\e[38;5;184m/\e[38;5;124m_\e[38;5;184m/\e[38;5;124m__"
+        echo -e "    |_______|"
+        echo -e "     \     /"
+        echo -e "      \___/\e[39m\e[00m"
+        echo -e ""
     end
 end
