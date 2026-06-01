188bf7ae8451d9983b35721368d4fb84fec4c64c
Dan Sully <dsully@users.noreply.github.com>
2026-06-01 14:16:13 -0700

    chore(javascript): add npm XDG-compliant config and global bin path
    - Define `npmCacheDir` pointing to `$XDG_CACHE_HOME/npm`
    - Add npm cache and global prefix dirs to `sessionPath`
    - Set `NPM_CONFIG_CACHE`, `NPM_CONFIG_PREFIX`, `NPM_CONFIG_TMP`, and `NPM_CONFIG_USERCONFIG` to follow XDG base dir spec

--- modules/home/packages/javascript.nix
+++ modules/home/packages/javascript.nix
@@ -11,6 +11,8 @@
       BUN_INSTALL="${bunInstallPath}" ${lib.getExe pkgs.bun} install -g "${pkg}" >/dev/null 2>&1 || echo "Warning: Failed to install ${pkg}"
     '')
     config.packageTools.javascript;
+
+  npmCacheDir = "${config.xdg.cacheHome}/npm";
 in {
   options.packageTools.javascript = lib.mkOption {
     type = lib.types.listOf lib.types.str;
@@ -20,11 +22,22 @@ in {
 
   config = lib.mkIf (config.packageTools.javascript != [] && pkgs.stdenv.isDarwin) {
     home = {
-      sessionVariables.BUN_INSTALL = bunInstallPath;
-      sessionPath = ["${bunInstallPath}/bin"];
       activation.npmTools = lib.hm.dag.entryAfter ["writeBoundary" "installPackages"] ''
         ${installScript}
       '';
+
+      sessionPath = [
+        "${bunInstallPath}/bin"
+        "${npmCacheDir}/bin"
+      ];
+
+      sessionVariables = {
+        BUN_INSTALL = bunInstallPath;
+        NPM_CONFIG_CACHE = npmCacheDir;
+        NPM_CONFIG_PREFIX = npmCacheDir;
+        NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+        NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/config";
+      };
     };
   };
 }
