a9eacd3ce8cb961f6bfe30d2dcd090183440bc55
Dan Sully <dsully@users.noreply.github.com>
2026-07-25 12:52:26 -0700

    chore: update versions

--- packages/caddy-custom.nix
+++ packages/caddy-custom.nix
@@ -13,7 +13,7 @@ then
       "github.com/abiosoft/caddy-inspect@v0.0.0-20250214103948-96cdb1dfb122"
       "github.com/abiosoft/caddy-json-schema@v0.0.0-20220621031927-c4d6e132f3af"
     ];
-    hash = "sha256-3Bfo87SC6eQlq0oDDQS78TRJuB8i9exWTKdVG3iAnZM=";
+    hash = "sha256-4ijxYxoZ4rspQpq9yeTENHLc4MOHHx7H/WgPe9mPLLc=";
   }).overrideAttrs {
     doCheck = false;
   }
